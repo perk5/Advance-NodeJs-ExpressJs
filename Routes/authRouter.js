046829c84d76480653d992c41263d1a39747f8fe
Prerak Shah <prerak9350@gmail.com>
2025-07-14 00:53:18 +0930

    Done Updated Password functionality

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -10,5 +10,7 @@ router.route('/login').post(authController.login)
 router.route('/forgotPassword').post(authController.forgotPassword)
 
 router.route('/resetPassword/:token').patch(authController.resetPassword)
+
+router.route('/updatePassword').patch(authController.protect ,authController.updatePassword)
  
 module.exports = router
